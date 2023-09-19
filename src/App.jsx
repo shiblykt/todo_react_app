dfe5620a9625bdb9eb0e1effe958e901bbb57663
shibly <shibly@baabte.com>
2023-09-19 22:48:17 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Users from './users/pages/Users'
 import NewLocations from './locations/pages/NewLocations'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
 
 
 const App = () => {
 
   return (
   <>
+    <MainNavigation />
     <Router>
     <Routes>
     <Route path='*' exact element={<Users/>}></Route>
