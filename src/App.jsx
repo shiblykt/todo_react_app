a0531eace376eccdbc14c860a383b35049551242
shibly <shibly@baabte.com>
2023-09-27 22:11:43 +0530

    navigation in header updations

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import './App.css'
 import Users from './users/pages/Users'
 import NewLocations from './locations/pages/NewLocations'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
+import './App.css'
 
 
 const App = () => {
 
   return (
-  <>
-    <MainNavigation />
     <Router>
-    <Routes>
-    <Route path='*' exact element={<Users/>}></Route>
-    <Route path='/locations' exact element={<NewLocations/>}/>
-    </Routes>
+      <MainNavigation />
+      <Routes>
+        <Route path='*' element={<Users/>}></Route>
+        <Route path='/locations' exact element={<NewLocations/>}/>
+      </Routes>
     </Router>
-
-  </>
-    
   )
 }
 
--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -3,20 +3,21 @@ import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
+import NavLinks from './NavLink';
 
-const MainNavigation = props => {
+const MainNavigation = (props) => {
   return (
     <MainHeader>
       <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
+        <span></span>
+        <span></span>
+        <span></span>
       </button>
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
       <nav>
-        ...
+        <NavLinks />
       </nav>
     </MainHeader>
   );
